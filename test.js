afc9f718400261d96c051236d0ba857845e80b42
ChicoFernandess <franciscofeernandes96@hotmail.com>
2024-12-24 09:39:50 +0000

    save score with local storage and add game messages to the frontend

--- test.js
+++ test.js
@@ -1,43 +1,59 @@
 const answersArray = ["paper","rock","scissor"];
 let meCounter = 0;
 let botCounter = 0;
+let mePointsStorage = parseInt(localStorage.getItem("mePoints")) || 0;
+let botPointsStorage = parseInt(localStorage.getItem("botPoints")) || 0;
+
+function displayScore(){
+    document.getElementById("scoreId").innerHTML = `score: ${mePointsStorage} - ${botPointsStorage}`;
+}
 
 function getAnswer(value){
     console.log("Me: " , value);
     const botAnswer = getBotAnswer();
-    console.log("Bot :" , botAnswer)
+    console.log("Bot :" , botAnswer);
+    let message = "";
     //console.log(getWinner());
     if(value == "paper" && botAnswer == "rock"){
         console.log("Me wins with paper");
-        meCounter++;
+        message = "Me wins with paper";
+        mePointsStorage++;
     }
     if(value == "rock" && botAnswer == "scissor"){
         console.log("Me wins with rock");
-        meCounter++;
+        message = "Me wins with rock";
+        mePointsStorage++;
     }
     if(value == "scissor" && botAnswer == "paper"){
         console.log("Me wins with scissor");
-        meCounter++;
+        message = "Me wins with scissor";
+        mePointsStorage++;
     }
     if(value == "paper" && botAnswer == "scissor"){
         console.log("Bot wins with scissor");
-        botCounter++;
+        message = "Bot wins with paper";
+        botPointsStorage++;
     }
     if(value == "rock" && botAnswer == "paper"){
         console.log("Bot wins with rock");
-        botCounter++;
+        message = "Bot wins with rock";
+        botPointsStorage++;
     }
     if(value == "scissor" && botAnswer == "rock"){
         console.log("Bot wins with scissor");
-        botCounter++;
+        message = "Bot wins with scissor";
+        botPointsStorage++;
     }
 
-    let mePoints = meCounter;
-    let botPoints = botCounter;
 
-    console.log("score:" , mePoints , "-" , botPoints);
+    localStorage.setItem("mePoints", mePointsStorage);
+    localStorage.setItem("botPoints",botPointsStorage);
 
-    document.getElementById("scoreId").innerHTML = `score: ${mePoints} - ${botPoints}`;
+    console.log("score:" , mePointsStorage , "-" , botPointsStorage);
+
+    document.getElementById("messages").innerHTML = `${message}`;
+
+    displayScore();
 }
 
 function getBotAnswer(){
@@ -61,3 +77,4 @@ function getWinner(){
     return message;
 
 }
+displayScore();
